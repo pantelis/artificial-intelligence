f6a077c55807e940f16eff147abd376e30fe3c22
Pantelis Monogioudis <508740+pantelis@users.noreply.github.com>
2022-02-04 12:02:37 -0500

    updated toc and config

--- conf.py
+++ conf.py
@@ -3,27 +3,26 @@
 # If you wish to continue using _config.yml, make edits to that file and
 # re-generate this one.
 ###############################################################################
-author = 'Pantelis Monogioudis, Ph.D'
-bibtex_bibfiles = ['references.bib']
-comments_config = {'hypothesis': False, 'utterances': True}
+author = 'The Jupyter Book community'
+comments_config = {'hypothesis': False, 'utterances': False}
 copyright = '2021'
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
 execution_allow_errors = False
 execution_excludepatterns = []
 execution_in_temp = False
 execution_timeout = 30
-extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_panels', 'sphinx_book_theme', 'sphinxcontrib.youtube', 'sphinx_inline_tabs', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
+extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_panels', 'sphinx_book_theme', 'sphinx_jupyterbook_latex']
 external_toc_exclude_missing = False
 external_toc_path = '_toc.yml'
-html_baseurl = 'https://pantelis.github.io/artificial-intelligence'
+html_baseurl = ''
 html_favicon = ''
-html_logo = 'logo.png'
+html_logo = ''
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
-html_theme_options = {'use_download_button': False, 'repository_url': 'https://pantelis.github.io/artificial-intelligence', 'use_issues_button': True, 'use_repository_button': False, 'use_edit_page_button': False}
-html_title = 'Introduction to Artificial Intelligence'
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': '', 'repository_url': 'https://github.com/executablebooks/jupyter-book', 'repository_branch': 'master', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'use_repository_button': False, 'use_edit_page_button': False, 'use_issues_button': False}
+html_title = 'My Jupyter Book'
 jupyter_cache = ''
-jupyter_execute_notebooks = 'off'
+jupyter_execute_notebooks = 'auto'
 language = None
 latex_engine = 'pdflatex'
 myst_enable_extensions = ['colon_fence', 'dollarmath', 'linkify', 'substitution', 'tasklist']
