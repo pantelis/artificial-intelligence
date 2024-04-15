d000199d334dd5bce76c3da087f5f6e7403f8a6b
Pantelis Monogioudis <pantelis.monogioudis@aegean.ai>
2024-04-15 09:12:57 -0400

    updated pytorch container to work with pip-tools and corrected github workflow

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,26 @@
+# Description
+
+*Please include a summary of the change and which issue is fixed (if any). Please also
+include relevant motivation and context. List any dependencies that are required for
+this change.*
+
+Fixes # (issue)
+
+## Type of change
+
+- [ ] Documentation (non-breaking change that adds or improves the documentation)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Optimization (non-breaking, back-end change that speeds up the code)
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] Breaking change (whatever its nature)
+
+## Key checklist
+
+- [ ] All tests pass (eg. `python -m pytest`)
+- [ ] The documentation builds and looks OK (eg. `python -m sphinx -b html docs docs/build`)
+- [ ] Pre-commit hooks run successfully (eg. `pre-commit run --all-files`)
+
+## Further checks
+
+- [ ] Code is commented, particularly in hard-to-understand areas
+- [ ] Tests added or an issue has been opened to tackle that in the future. (Indicate issue here: # (issue))
--- requirements/tasks.py
+++ requirements/tasks.py
@@ -1,4 +1,6 @@
 # https://gabnotes.org/pip-tools-for-python-dependencies-management/
+# Please note that hash generation was removed as we need to install local packages
+
 # tasks.py
 from pathlib import Path
 
@@ -6,6 +8,28 @@ from invoke import Context, task
 
 BASE_DIR = Path(__file__).parent.resolve(strict=True)
 
+@task
+def split_dependency_constraints(ctx: Context)->None:
+    return separate_local_constraints(ctx, 'constraints.txt', 'local_constraints.txt', 'other_constraints.txt')
+
+def separate_local_constraints(ctx:Context, input_file, output_file1, output_file2)->None:
+    with ctx.cd(BASE_DIR):
+        
+        with open(input_file, 'r') as f:
+            local_constraints = []
+            other_constraints = []
+            for line in f:
+                if '@ file://' in line:
+                    local_constraints.append(line)
+                else:
+                    other_constraints.append(line)
+        with open(output_file1, 'w') as f:
+            f.writelines(local_constraints)
+        with open(output_file2, 'w') as f:
+            f.writelines(other_constraints)
+    
+        
+        
 @task
 def update_dependencies(ctx: Context, *, sync: bool = True) -> None:
     return compile_dependencies(ctx, update=True, sync=sync)
@@ -19,17 +43,13 @@ def update_dev_dependencies(ctx: Context, *, sync: bool = True) -> None:
 def compile_dependencies(
     ctx: Context, *, update: bool = False, sync: bool = False
 ) -> None:
-    common_args = "--allow-unsafe --resolver=backtracking"
+    common_args = "--resolver=backtracking"
     if update:
         common_args += " --upgrade"
     with ctx.cd(BASE_DIR):
+        
         ctx.run(
-            f"pip-compile {common_args} --strip-extras --reuse-hashes --generate-hashes requirements.in",
-            pty=True,
-            echo=True,
-        )
-        ctx.run(
-            f"pip-compile {common_args} --strip-extras --reuse-hashes -o constraints.txt requirements.in",
+            f"pip-compile {common_args}  --index-url https://pypi.nvidia.com/simple --extra-index-url https://pypi.org/simple --trusted-host pypi.nvidia.com --reuse-hashes --output-file requirements.txt docs.requirements.in  --constraint other_constraints.txt requirements.in", 
             pty=True,
             echo=True,
         )
@@ -41,19 +61,24 @@ def compile_dependencies(
 def compile_dev_dependencies(
     ctx: Context, *, update: bool = False, sync: bool = False
 ) -> None:
-    common_args = "--allow-unsafe --resolver=backtracking"
+    common_args = "--resolver=backtracking"
     if update:
         common_args += " --upgrade"
     with ctx.cd(BASE_DIR):
         ctx.run(
-            f"pip-compile {common_args} --strip-extras --reuse-hashes --generate-hashes requirements-dev.in",
+            f"pip-compile {common_args} --index-url https://pypi.nvidia.com/simple --extra-index-url https://pypi.org/simple --trusted-host pypi.nvidia.com --reuse-hashes dev.requirements.in", # --generate-hashes --reuse-hashes
             pty=True,
             echo=True,
         )
     if sync:
-        sync_dependencies(ctx)
+        sync_dev_dependencies(ctx)
 
 @task
 def sync_dependencies(ctx: Context) -> None:
     with ctx.cd(BASE_DIR):
-        ctx.run("pip-sync requirements.txt requirements-dev.txt", pty=True, echo=True)
+        ctx.run("pip-sync requirements.txt", pty=True, echo=True)
+        
+@task
+def sync_dev_dependencies(ctx: Context) -> None:
+    with ctx.cd(BASE_DIR):
+        ctx.run("pip-sync requirements-dev.txt", pty=True, echo=True)
